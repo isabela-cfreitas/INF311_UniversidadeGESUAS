1107bf1fa37ffa210cb881973e21cf5666621e55
Isabela <129128103+isabela-cfreitas@users.noreply.github.com>
2026-05-27 09:15:47 -0300

    nome de usuario e cargo no cadastro

--- app/src/main/java/com/UniversidadeGESUAS_INF311/CadastroActivity.java
+++ app/src/main/java/com/UniversidadeGESUAS_INF311/CadastroActivity.java
@@ -20,7 +20,7 @@ public class CadastroActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
-    private EditText nome,dia,mes,ano,cpf,email,senha,senha_conf;
+    private EditText nome,nome_usuario,dia,mes,ano,cpf,email,senha,senha_conf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +30,7 @@ public class CadastroActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         nome = (EditText) findViewById(R.id.nome_res);
+        nome_usuario = (EditText) findViewById(R.id.nome_usuario_res);
         dia = (EditText) findViewById(R.id.dia_res);
         mes = (EditText) findViewById(R.id.mes_res);
         ano = (EditText) findViewById(R.id.ano_res);
@@ -49,44 +50,56 @@ public class CadastroActivity extends AppCompatActivity {
     }
 
     public void click_cadastrar (View v) {
-        String email_ = email.getText().toString();
+        String email_ = email.getText().toString().trim();
         String senha_ = senha.getText().toString();
         String senha_conf_ = senha_conf.getText().toString();
-        String nome_ = nome.getText().toString();
-        String dia_ = dia.getText().toString();
-        String mes_ = mes.getText().toString();
-        String ano_ = ano.getText().toString();
-        String cpf_ = cpf.getText().toString();
+        String nome_ = nome.getText().toString().trim();
+        String nome_usuario_ = nome_usuario.getText().toString().trim();
+        String dia_ = dia.getText().toString().trim();
+        String mes_ = mes.getText().toString().trim();
+        String ano_ = ano.getText().toString().trim();
+        String cpf_ = cpf.getText().toString().trim();
         String nasc_ = dia.getText().toString() + "/" + mes.getText().toString() + "/" + ano.getText().toString();
-        if (validar_dados(nome_, dia_, mes_, ano_, cpf_, email_, senha_, senha_conf_)) {
-            mAuth.createUserWithEmailAndPassword(email_, senha_).addOnCompleteListener(this, task -> {
-                if (task.isSuccessful()) {
-                    salvar_dados(task.getResult().getUser().getUid(), nome_, cpf_, nasc_);
+        if (validar_dados(nome_,nome_usuario_, dia_, mes_, ano_, cpf_, email_, senha_, senha_conf_)) {
+
+            db.collection("Usuarios").whereEqualTo("nome_usuario", nome_usuario_).get().addOnCompleteListener(task -> {
+                if (task.isSuccessful() && !task.getResult().isEmpty()) {
+                    Toast.makeText(this, "Este nome de usuário já está em uso.", Toast.LENGTH_SHORT).show();
                 } else {
-                    String erro = task.getException().getMessage();
-                    Toast.makeText(this, "Erro: " + erro, Toast.LENGTH_LONG).show();
+                    mAuth.createUserWithEmailAndPassword(email_, senha_).addOnCompleteListener(this, task2 -> {
+                        if (task2.isSuccessful()) {
+                            salvar_dados(task2.getResult().getUser().getUid(), nome_, nome_usuario_, cpf_, nasc_);
+                        } else {
+                            String erro = task2.getException().getMessage();
+                            Toast.makeText(this, "Erro: " + erro, Toast.LENGTH_LONG).show();
+                        }
+                    });
                 }
             });
         }
     }
 
-    private Boolean validar_dados (String nome_,String dia_,String mes_,String ano_,String cpf_,String email_,String senha_,String senha_conf_) {
+    private Boolean validar_dados (String nome_,String nome_usuario_,String dia_,String mes_,String ano_,String cpf_,String email_,String senha_,String senha_conf_) {
         boolean ok = false;
-        if (email_.isEmpty() || senha_.isEmpty() || senha_conf_.isEmpty() || nome_.isEmpty() || dia_.isEmpty() || mes_.isEmpty() || ano_.isEmpty() || cpf_.isEmpty()) {
+        if (email_.isEmpty() || senha_.isEmpty() || senha_conf_.isEmpty() || nome_.isEmpty() || dia_.isEmpty() || mes_.isEmpty() || ano_.isEmpty() || cpf_.isEmpty() || nome_usuario_.isEmpty()) {
             Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
         } else if (!senha_.equals(senha_conf_)){
             Toast.makeText(this, "Senha ou confirmação de senha incorreta", Toast.LENGTH_SHORT).show();
         } else if (cpf_.length()!=11) {
             Toast.makeText(this, "Informe um cpf válido", Toast.LENGTH_SHORT).show();
+        } else if (!nome_usuario_.matches("^[a-zA-Z0-9_]+$")) {
+            Toast.makeText(this, "O nome de usuário deve conter apenas letras, números e underlines (_)", Toast.LENGTH_SHORT).show();
         } else {
             ok = true;
         }
         return ok;
     }
 
-    private void salvar_dados(String id_, String nome_, String cpf_, String nasc_) {
+    private void salvar_dados(String id_, String nome_,String nome_usuario_, String cpf_, String nasc_) {
         Map<String, Object> usuario = new HashMap<>();
         usuario.put("nome", nome_);
+        usuario.put("nome_usuario",nome_usuario_);
+        usuario.put("cargo", "estudante");
         usuario.put("cpf", cpf_);
         usuario.put("dataNascimento", nasc_);
         usuario.put("pontos", 0);
